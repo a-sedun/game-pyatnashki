d669c184cb2bdd66e80a7ff2dcf56e86e58a44a4
Anastasia <snastia004@gmail.com>
2024-02-19 13:36:03 +0200

    Added the function that will draw pyatnashki

--- game.py
+++ game.py
@@ -20,3 +20,34 @@ canvas = Canvas(
 canvas.pack()
 root.mainloop()
 board = list(range(1, EMPTY_SQUARE + 1))
+
+
+def draw_board():
+    # Прибираємо все, що  нарисоване в області для малювання
+    canvas.delete("all")
+    # Наша задача згрупувати п’ятнашки зі списку у квадрат
+    # розміром  BOARD_SIZE x BOARD_SIZE
+    # i та j будуть координатами для кожної окремої п’ятнашки
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            # Отримуємо значення для його малювання на квадраті
+            index = str(board[BOARD_SIZE * i + j])
+            # Якщо це не клітинка, яку необхідно залишити порожньою
+            if index != str(EMPTY_SQUARE):
+                # Малюємо квадрат по заданним координатам
+                canvas.create_rectangle(
+                    j * SQUARE_SIZE,
+                    i * SQUARE_SIZE,
+                    j * SQUARE_SIZE + SQUARE_SIZE,
+                    i * SQUARE_SIZE + SQUARE_SIZE,
+                    fill="#43ABC9",
+                    outline="#FFFFFF",
+                )
+                # Пишемо число у центрі квадрата
+                canvas.create_text(
+                    j * SQUARE_SIZE + SQUARE_SIZE / 2,
+                    i * SQUARE_SIZE + SQUARE_SIZE / 2,
+                    text=index,
+                    font="Arial {} italic".format(int(SQUARE_SIZE / 4)),
+                    fill="#FFFFFF",
+                )
